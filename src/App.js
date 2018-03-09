9d14568b40fd741ba79558c85d160aab58438df9
utsushiiro <ozeki.haru@gmail.com>
2018-03-09 22:17:13 +0900

    トップページのルーティングを設定

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import Ranking from './components/Ranking';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello React</div>
+      <div className={"App"}>
+        <ul>
+          <li><Link to={"/all"}>すべてのカテゴリ</Link></li>
+          <li><Link to={"/category/2502"}>パソコン、周辺機器</Link></li>
+          <li><Link to={"/category/10002"}>本、雑誌、コミック</Link></li>
+        </ul>
+        <Route path={"/all"} component={Ranking} />
+        <Route path={"/category/:id"} render={ (props) => <Ranking categoryId={props.match.params.id}/> }/>
+      </div>
     );
   }
 }
