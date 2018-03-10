78d0edd1cdb69d70920acdea05b26047c8a3f41b
utsushiiro <ozeki.haru@gmail.com>
2018-03-10 21:03:07 +0900

    Rankingのreducerを作成

---
+++ src/reducers/Ranking.js
@@ -0,0 +1,41 @@
+const initialState = {
+  categoryId: undefined,
+  ranking: undefined,
+  error: false
+};
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    case 'START_REQUEST':
+      return {
+        categoryId: action.payload.categoryId,
+        ranking: undefined,
+        error: false
+      };
+    case 'RECEIVE_REQUEST':
+      return action.payload.error
+      ? { ...state, error: true }
+      : {
+          ...state,
+          ranking: getRanking(action.payload.response)
+        };
+    default:
+      return state;
+  }
+}
+
+const getRanking = response => {
+  const resultSet = response.ResultSet;
+  const ranking = [];
+  const itemLength = resultSet.totalResultsReturned;
+  for (let i = 0; i < itemLength; i++) {
+    const item = resultSet['0'].Result[String.valueOf(i)];
+    ranking.push({
+      code: item.Code,
+      name: item.Name,
+      url: item.Url,
+      imageUrl: item.Image.Medium
+    })
+  }
+  return ranking;
+};
