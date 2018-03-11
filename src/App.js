3bd1913f23c52deb8f8a2f312c02ca9abc5b0e70
utsushiiro <ozeki.haru@gmail.com>
2018-03-11 15:41:46 +0900

    AppBarでアプリ名表示を実装

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Ranking from './containers/Ranking';
 import Nav from './containers/Nav';
+import Reboot from 'material-ui/Reboot';
+import AppBar from 'material-ui/AppBar';
+import ToolBar from 'material-ui/Toolbar';
+import Typegraphy from 'material-ui/Typography';
+
 
 class App extends Component {
   render() {
     return (
       <div className={"App"}>
+        <Reboot />
+
+        <AppBar>
+          <ToolBar>
+            <Typegraphy type={"title"} color={"inherit"}>
+              Yahoo!ショッピングランキング
+            </Typegraphy>
+          </ToolBar>
+        </AppBar>
         <Nav />
 
         <Switch>
