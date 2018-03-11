39bc0b3191c91fbf377a07a4b51df457ed294048
utsushiiro <ozeki.haru@gmail.com>
2018-03-11 11:38:54 +0900

    Rankingコンポーネントの表示を変更

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -13,9 +13,32 @@ export default class Ranking extends React.Component {
   }
 
   render() {
+    const { category, ranking, error } = this.props;
     return (
       <div>
-        <h2>Rankingコンポーネント</h2>
+        <h2>{ typeof category !== 'undefined' ? `${category.name}のランキング` : '' }</h2>
+        {
+          (()=>{
+            if (error) {
+              return <p>エラーが発生しました。リロードしてください。</p>;
+            } else if (typeof ranking === 'undefined') {
+              return <p>読み込み中...</p>;
+            } else {
+              return (
+                <ol>
+                  {
+                    ranking.map(item => (
+                      <li key={`ranking-item-${item.code}`}>
+                        <img src={item.imageUrl} alt={item.name} />
+                        <a href={item.url} target="_blank">{item.name}</a>
+                      </li>
+                    ))
+                  }
+                </ol>
+              );
+            }
+          })()
+        }
         <p>カテゴリーID: {this.props.categoryId}</p>
       </div>
     );
@@ -25,9 +48,22 @@ export default class Ranking extends React.Component {
 Ranking.propTypes = {
   categoryId: PropTypes.string,
   onMount: PropTypes.func.isRequired,
-  onUpdate: PropTypes.func.isRequired
+  onUpdate: PropTypes.func.isRequired,
+  category: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+  }),
+  ranking: PropTypes.arrayOf(
+    PropTypes.shape({
+      code: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired
+    })
+  ),
+  error: PropTypes.bool
 };
 
-Ranking.defaultTypes = {
+Ranking.defaultProps = {
   categoryId: '1'
 };
